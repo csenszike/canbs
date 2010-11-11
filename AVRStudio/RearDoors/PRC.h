2cde80db3092aae81ce3e869a4d9276278c88201
fulibl@gmail.com <fulibl@gmail.com@57441035-aaff-9e30-b066-25e1b4fe9b8f>
2010-11-11 10:06:54 +0000

    Frissítés az új üzenetformátumok és a Gergő kódja alapján.

--- AVRStudio/RearDoors/PRC.h
+++ AVRStudio/RearDoors/PRC.h
@@ -1,14 +1,35 @@
+/*
+	PRC driver
+*/
+/*=================================================[ COMPILER SWITCH CHECK ]=*/
+
+/*===========================================================[ HEADER INIT ]=*/
 #ifndef PRC_H
 #define PRC_H
+/*=======================================================[ EXTERNAL MACROS ]=*/
+/* macro constants and read macros */
 
-#define PRC_U16_FILTER1 0x120
-#define PRC_U16_FILTER2 0x120
+#define PRC_U16_FILTER1		0x0120
+#define PRC_U16_FILTER2		0x0120
+
+/*=====================================================[ EXTERNAL TYPEDEFS ]=*/
+/* State Machine Constants */
+/*======================================================[ EXTERNAL GLOBALS ]=*/
+/* Just for debug !!!!*/
+
+extern CAN_st_message_t PRC_stm_tx_message;
+
+/*=========================================[ EXTPORTED FUNCTION PROTOTYPES ]=*/
 
 extern void PRC_v_init_f(void);
 extern void PRC_v_refresh_control_f(void);
 extern void PRC_v_process_f(void);
 extern void PRC_v_refresh_status_f(void);
 
-extern CAN_st_message_t PRC_stm_tx_message;
+#endif /* PRC_H */
+/*
+ *===========================================================================*
+ * End of File                                                         
+ *===========================================================================*
+*/
 
-#endif
